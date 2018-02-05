624a7fae851ec81fb12cb0dd78ab930ad752036d
wanghan <942732983@qq.com>
2018-02-05 16:51:33 +0800

    第五次提交

---
+++ app/src/main/java/example/wanghan/com/broadcastdemo/BootCompleteReceiver.java
@@ -0,0 +1,22 @@
+package example.wanghan.com.broadcastdemo;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class BootCompleteReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+       Toast.makeText(context,"没什么意思，就是想让你看到，你懂了吧 ",Toast.LENGTH_SHORT).show();
+
+
+        Intent MainIntent=new Intent(context,MainActivity.class);
+        MainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(MainIntent);
+        throw new UnsupportedOperationException("Not yet implemented");
+
+    }
+}
